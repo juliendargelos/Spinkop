4060ace0b162ad59e4968c853d397f931b674947
Julien Dargelos <julien.dargelos@me.com>
2016-10-11 17:35:01 +0200

    JS and CSS form the home header, update assets

--- public/assets/pages-home-f78fb8bd7e214d5b978c5a82ca6db9aab5b0d11ca551607d3e4db6056e729b3e.js
+++ public/assets/pages-home-f78fb8bd7e214d5b978c5a82ca6db9aab5b0d11ca551607d3e4db6056e729b3e.js
@@ -1,5 +1,31 @@
-//=require components/eventsManager
-//=require components/scroll
+var eventsManager = {
+	on: function(event, callback, element, useCapture) {
+		element.addEventListener(event, callback, useCapture === undefined ? false : useCapture);
+	},
+	no: function(event, callback, element) {
+		element.removeEventListener(event, callback);
+	},
+	apply: function(element) {
+		element.on = function(event, callback) {window.eventsManager.on(event, callback, this);};
+		element.no = function(event, callback) {window.eventsManager.no(event, callback, this);};
+	},
+	init: function() {
+		this.apply(window);
+		this.apply(Node.prototype);
+	}
+};
+
+eventsManager.init();
+var scroll = {
+	get top() {
+		return document.body.scrollTop || document.documentElement.scrollTop;
+	},
+    set top(value) {
+		document.body.scrollTop = document.documentElement.scrollTop = value;
+	}
+};
+
+
 
 var main = {
 	header: {
